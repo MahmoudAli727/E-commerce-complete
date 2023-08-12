d7f7c7135b248dbeae52970c0f92787bab254e9a
mahmoud ali <mahmoudalio353@gmail.com>
2023-08-12 09:41:38 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_way/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_way
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routes.dart
@@ -0,0 +1,28 @@
+// We use name route
+// All our routes will be available here
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/cart/cart_screen.dart';
+import 'package:shop_app/screens/complete_profile/complete_profile_screen.dart';
+import 'package:shop_app/screens/details/details_screen.dart';
+import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
+import 'package:shop_app/screens/home/home_screen.dart';
+import 'package:shop_app/screens/login_success/login_success_screen.dart';
+import 'package:shop_app/screens/otp/otp_screen.dart';
+import 'package:shop_app/screens/profile/profile_screen.dart';
+import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
+import 'package:shop_app/screens/sign_up/sign_up_screen.dart';
+import 'package:shop_app/screens/splash/splash_screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
+  LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
+  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen(),
+  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
+  OtpScreen.routeName: (context) => OtpScreen(),
+  HomeScreen.routeName: (context) => HomeScreen(),
+  DetailsScreen.routeName: (context) => DetailsScreen(),
+  CartScreen.routeName: (context) => CartScreen(),
+  ProfileScreen.routeName: (context) => ProfileScreen(),
+};
